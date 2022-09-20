e124b88642a28bbf9791c347ec1264abcec36308
Tom Dudfield <TomDudfield@users.noreply.github.com>
2022-09-20 22:24:03 +0100

    update header

--- next.config.js
+++ next.config.js
@@ -38,7 +38,7 @@ const securityHeaders = [
   },
   {
     key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin'
+    value: 'strict-origin-when-cross-origin'
   },
   // {
   //   key: 'Content-Security-Policy',
