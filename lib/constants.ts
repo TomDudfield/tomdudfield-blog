5450668d0357e2f5a94be0f01ff92d17011f9e7f
Tom Dudfield <TomDudfield@users.noreply.github.com>
2022-09-20 21:52:07 +0100

    update desc

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 export const BLOG_NAME = 'Tom Dudfield'
-export const BLOG_DESC = 'Sitecore Architect'
+export const BLOG_DESC = 'My tech notes'
 export const BLOG_SUMMARY = 'A blog about developing and architecting solutions on the Sitecore platform and other related technologies.'
