fa3602082477fc5951b3951c7216a5ecf010476a
Tom Dudfield <TomDudfield@users.noreply.github.com>
2022-09-22 22:44:35 +0100

    revert

--- next.config.js
+++ next.config.js
@@ -16,30 +16,30 @@ const securityHeaders = [
     key: 'X-DNS-Prefetch-Control',
     value: 'on'
   },
-  // {
-  //   key: 'Strict-Transport-Security',
-  //   value: 'max-age=31536000; includeSubDomains; preload'
-  // },
-  // {
-  //   key: 'X-XSS-Protection',
-  //   value: '1; mode=block'
-  // },
-  // {
-  //   key: 'X-Frame-Options',
-  //   value: 'SAMEORIGIN'
-  // },
-  // {
-  //   key: 'Permissions-Policy',
-  //   value: 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()'
-  // },
-  // {
-  //   key: 'X-Content-Type-Options',
-  //   value: 'nosniff'
-  // },
-  // {
-  //   key: 'Referrer-Policy',
-  //   value: 'strict-origin-when-cross-origin'
-  // },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin'
+  },
   // {
   //   key: 'Content-Security-Policy',
   //   value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
