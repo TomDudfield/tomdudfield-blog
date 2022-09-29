68e50a5eb74bd02f613fd1f763e6f9c3dcc0270b
Tom Dudfield <TomDudfield@users.noreply.github.com>
2022-09-29 22:09:45 +0100

    csp

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 else {
   contentSecurityPolicy += `
-  style-src 'self'; 
+  style-src 'self' 'unsafe-inline'; 
   script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://static.cloudflareinsights.com`
 }
 
