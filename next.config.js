9a49913316d3fd3fbffb00c640212c431948c87d
Tom Dudfield <TomDudfield@users.noreply.github.com>
2022-09-29 22:03:14 +0100

    csp

--- next.config.js
+++ next.config.js
@@ -1,5 +1,25 @@
 // @ts-check
 
+let contentSecurityPolicy = `
+  default-src 'self';
+  img-src 'self' data: https://www.google.co.uk;
+  font-src 'self' data:;
+  connect-src 'self' https://*.analytics.google.com https://vitals.vercel-insights.com https://stats.g.doubleclick.net;
+  form-action 'none';
+  frame-ancestors 'none';
+  report-uri https://dudfield.report-uri.com/r/d/csp/wizard`
+
+if (process.env.NODE_ENV !== 'production') {
+  contentSecurityPolicy += `
+  style-src 'self' 'unsafe-inline'; 
+  script-src 'unsafe-eval' 'unsafe-inline' 'self' https://www.googletagmanager.com https://static.cloudflareinsights.com`
+}
+else {
+  contentSecurityPolicy += `
+  style-src 'self'; 
+  script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://static.cloudflareinsights.com`
+}
+
 /**
  * @type {import('next').NextConfig}
  **/
@@ -31,6 +51,10 @@ const securityHeaders = [
   {
     key: 'Referrer-Policy',
     value: 'strict-origin-when-cross-origin'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
   }
 ]
 
