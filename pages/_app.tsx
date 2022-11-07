128ef74fde4c4109f639f6b2b190b8f37f335a01
Tom Dudfield <TomDudfield@users.noreply.github.com>
2022-11-07 22:00:20 +0000

    enable SSG

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/index.css'
 import { NextWebVitalsMetric } from 'next/app'
 import React from 'react';
-import App from "next/app"
 import Script from 'next/script';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-function MyApp({ Component, pageProps, canonical }) {
+function MyApp({ Component, pageProps }) {
+  const isProd = process.env.NODE_ENV === "production";
+  const base = isProd ? "https://www.tomdudfield.com" : "http://localhost:3000";
+  
+  const getCanonical = (path: string) => {
+    const fullURL = new URL(path, base)
+    return`${fullURL.origin}${fullURL.pathname}`
+  }
+  const router = useRouter()
+  const canonical = getCanonical(router.asPath)
   return (
     <>
       <Head>
@@ -34,19 +43,6 @@ function MyApp({ Component, pageProps, canonical }) {
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
   console.log(metric)
-}
-
-MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
-  const isProd = process.env.NODE_ENV === "production";
-  const base = isProd ? "https://www.tomdudfield.com" : "http://localhost:3000";
-  const { asPath } = appContext.ctx;
-  const canonical = base + asPath;
-
-  return {
-    ...appProps,
-    canonical
-  }
 }
 
 export default MyApp;
